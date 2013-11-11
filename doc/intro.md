f8534a17e90994f66f6162633c154d3d4b8ae1b5
Logan Campbell <colin@logaan.net>
2013-11-11 20:45:33 +1100

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljdeferred
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cljdeferred/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljdeferred.core-test
+  (:require [clojure.test :refer :all]
+            [cljdeferred.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
